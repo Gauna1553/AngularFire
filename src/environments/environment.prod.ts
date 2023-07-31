a79bd6456858c556c1e5699d82ec91f99b19b2a4
Gauna1553 <gabrielgauna04@gmail.com>
2023-07-31 10:00:39 -0300

    implemente la base de datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyA8bFX5aL3x6fneJ-QkHlPnC3IRFHGj3Ow",
+        authDomain: "angularfire-bfd43.firebaseapp.com",
+        projectId: "angularfire-bfd43",
+        storageBucket: "angularfire-bfd43.appspot.com",
+        messagingSenderId: "968005529064",
+        appId: "1:968005529064:web:973757a0807a77a55a885d"
+      }
 }
