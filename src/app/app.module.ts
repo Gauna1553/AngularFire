391d98700e352b74580d7c926912683f26063861
Gauna1553 <gabrielgauna04@gmail.com>
2023-07-31 10:06:38 -0300

    importamos dos libreriras de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { AngularFireModule } from '@angular/fire/compat'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
